5052bf43adf335eb5c16fd0bb1ec709f28af511f
Pubudu Ranasinghe <pubudutr@gmail.com>
2019-11-02 22:37:04 +0530

    Setup basic todo context.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { ItemList, NewItem } from "./components/Items";
+import TodoContext from "./contexts/TodoContext";
 import "./App.css";
 
-const initialItems = ["Setup basic components", "Add some styling"];
+const initialItems = [
+  "Setup todo context",
+  "Consume context from child components"
+];
 
 function App() {
-  // useState hook returns two values. First is the state itself
-  // and second is a function that we can use to update the state
   const [items, setItems] = useState(initialItems);
 
   function handleAddItem(item) {
@@ -19,14 +21,20 @@ function App() {
     setItems(copy);
   }
 
+  // We wrap the entire application with TodoContext provider, so that it
+  // _provides_ the value defined here(items) to the entire component heirachy
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>🚀 ToDo App</h2>
-        <NewItem add={handleAddItem} />
-        <ItemList items={items} remove={handleRemoveItem} />
-      </header>
-    </div>
+    <TodoContext.Provider
+      value={{ items, add: handleAddItem, remove: handleRemoveItem }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <h2>🚀 ToDo App</h2>
+          <NewItem />
+          <ItemList />
+        </header>
+      </div>
+    </TodoContext.Provider>
   );
 }
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
+import TodoContext from "../contexts/TodoContext";
 
 export function NewItem({ add }) {
   const [text, setText] = useState("");
 
   return (
-    <div className="Item">
-      <input
-        type="text"
-        placeholder="New Task"
-        value={text}
-        onChange={e => setText(e.target.value)}
-      ></input>
-      <button onClick={() => add(text)}>Add</button>
-    </div>
+    <TodoContext.Consumer>
+      {values => (
+        <div className="Item">
+          <input
+            type="text"
+            placeholder="New Task"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          ></input>
+          <button onClick={() => values.add(text)}>Add</button>
+        </div>
+      )}
+    </TodoContext.Consumer>
   );
 }
 
-export function ItemList({ items, remove }) {
-  return items.map((item, i) => <Item text={item} index={i} key={i} remove={remove} />);
+export function ItemList() {
+  // Here we consume the values from TodoContext using the TodoContext Consumer
+  return (
+    <TodoContext.Consumer>
+      {values =>
+        values.items.map((item, i) => (
+          <Item text={item} index={i} key={i} remove={values.remove} />
+        ))
+      }
+    </TodoContext.Consumer>
+  );
 }
 
 export function Item({ text, index, remove }) {
