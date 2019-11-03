5200fa9f8f64957b8d529ed7fcd4e4c2bfbfcf83
Pubudu Ranasinghe <pubudutr@gmail.com>
2019-11-03 18:32:26 +0530

    Add login functionality.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import React from "react";
 import Todos from "./components/Todos";
 import Login from "./components/Login";
 import { TodoProvider } from "./contexts/TodoContext";
+import { AuthProvider, useAuthContext, logout } from "./contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Greeting />
-        <Switch>
-          <Route path="/todos">
-            <TodoProvider>
-              <Todos />
-            </TodoProvider>
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Greeting />
+          <Switch>
+            <Route path="/todos">
+              <TodoProvider>
+                <Todos />
+              </TodoProvider>
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
 function Greeting() {
+  const { auth, dispatch } = useAuthContext();
+
+  if (auth.isLoggedIn)
+    return (
+      <p>
+        Hello, {auth.name}!
+        <button onClick={e => dispatch(logout())}>Logout</button>
+      </p>
+    );
   return <p>You are not logged in</p>;
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
+import {
+  useAuthContext,
+  loginSuccess,
+  loginFail
+} from "../contexts/AuthContext";
+import { apiLogin } from "../api/auth";
+import { Redirect } from "react-router-dom";
 
 export default function Login() {
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
+  const { auth, dispatch } = useAuthContext();
 
-  function handleLogin() {
-    // Handle login here
+  // Handle login will call the login API and dispatch actions
+  // depending on the result
+  async function handleLogin() {
+    setLoading(true);
+    try {
+      await apiLogin(name);
+      dispatch(loginSuccess(name));
+    } catch (error) {
+      dispatch(loginFail(error.message));
+    }
+    setLoading(false);
   }
+
+  // If user is already logged in redirect to Todos page
+  if (auth.isLoggedIn) return <Redirect to="/todos" />;
 
   if (loading) return <p>Loading..</p>;
 
@@ -22,6 +42,7 @@ export default function Login() {
         ></input>
         <button onClick={handleLogin}>Login</button>
       </div>
+      {auth.error && <p>{auth.error}</p>}
     </header>
   );
 }
